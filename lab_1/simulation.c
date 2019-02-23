425b9ce1673ddda0c4563e9c263422951e2c23c6
Hoang Ho <hoangho@Hoangs-MacBook-Pro.local>
2019-02-22 20:03:55 -0500

    add main files

---
+++ lab_1/simulation.c
@@ -0,0 +1,261 @@
+/*
+author: Hoang Ho
+Simulation File
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+#include "statistics.h"
+
+/*Handling functions*/
+
+int gen_next_event();
+int cpu_handler(Node **job);
+int disk_handler(Node **job);
+void enqueue_disk(int arrival_time, int id);
+int process_disk1(Node **job);
+int process_disk2(Node **job);
+void read_config();
+
+int SEED;
+int INIT_TIME;
+int FIN_TIME;
+int ARRIVE_MIN;
+int ARRIVE_MAX;
+float QUIT_PROB;
+int CPU_MIN;
+int CPU_MAX;
+int DISK1_MIN;
+int DISK1_MAX;
+int DISK2_MIN;
+int DISK2_MAX;
+
+Queue *event_queue = NULL;
+Queue *cpu_queue = NULL;
+Queue *disk1_queue = NULL;
+Queue *disk2_queue = NULL;
+
+int time_cpu_free = 0;
+int time_disk1_free = 0;
+int time_disk2_free = 0;
+int job_id = 0;
+
+/*
+SIMULATION
+*/
+
+int main(int argc, char *argv[])
+{
+    read_config(); // start the program by reading in the configurations
+    srand(SEED); 
+
+    /*
+    Generate the first job and enqueue the job into the event_queue and cpu_queue
+    */
+    int next = rand() % (ARRIVE_MAX - ARRIVE_MIN);
+    int current_time = next;
+    job_id += 1;
+
+    event_queue = newQueue();
+    enqueue(event_queue, next, next, job_id, 0);
+    cpu_queue = newQueue();
+    enqueue(cpu_queue, next, next, job_id, 0);
+
+    disk1_queue = newQueue();
+    disk2_queue = newQueue();
+
+    Node *temp = NULL;
+    int finished_time = 0;
+
+    start_log();
+
+    while (!isEmpty(event_queue) && current_time <= FIN_TIME)
+    {
+        /*
+        1. Pop an event out of the event_queue and generate the next event coming to the event queue
+        2. Check what server the event is coming in, and use the corresponding handler
+        3. If the current_time is smaller than the time the server finishes the event, set the current_time to 
+        finished_time
+        */
+        printf("Current Time: %d\n", current_time);
+        temp = pop(event_queue);
+        next = gen_next_event(next);
+        if (temp->server != 0)
+        {
+            finished_time = disk_handler(&temp);
+        }
+        else
+        {
+            finished_time = cpu_handler(&temp);
+        }
+        /* Need to update the current time */
+        if (current_time < finished_time)
+        {
+            current_time = finished_time;
+        }
+        free(temp);
+    }
+    calculate_statistics(INIT_TIME, FIN_TIME); // After the simulation, calculate the necessary statistics
+}
+
+/*
+Handler Functions
+1. Generate the Next Event
+2. CPU Handler
+3. Enqueue to disk
+4. Disk handler
+5. Process disk 1
+6. Process disk 2
+*/
+
+int gen_next_event(int last_arrival)
+{
+    job_id += 1;
+    int next_arrival = last_arrival + ((rand() % (ARRIVE_MAX - ARRIVE_MIN)) + ARRIVE_MIN);
+    enqueue(event_queue, next_arrival, time_cpu_free, job_id, 0);
+    enqueue(cpu_queue, next_arrival, time_cpu_free, job_id, 0);
+    return next_arrival;
+}
+
+int cpu_handler(Node **job)
+{
+    int current_time = (*job)->pop_time;
+    if (current_time < time_cpu_free)
+    {
+        current_time = time_cpu_free;
+        (*job)->pop_time = current_time;
+    }
+    int id = dequeue(cpu_queue);
+    int time_handling = (rand() % (CPU_MAX - CPU_MIN)) + CPU_MIN + 1;
+    time_cpu_free = current_time + time_handling; // update time_cpu_free
+
+    int response_time = time_cpu_free - ((*job)->arrival_time);
+    log_statistics((*job)->id, (*job)->server, (*job)->arrival_time, (*job)->pop_time, time_cpu_free,
+                   response_time, time_handling, subSize(cpu_queue, time_cpu_free));
+
+    if (cpu_queue->head)
+    {
+        cpu_queue->head->pop_time = time_cpu_free;
+    }
+
+    int diskNeeded = (rand() % 100) > (QUIT_PROB * 100);
+    if (diskNeeded)
+    {
+        enqueue_disk(time_cpu_free, id);
+    }
+    return time_cpu_free;
+}
+
+void enqueue_disk(int arrival_time, int id)
+{
+    if (disk1_queue->size > disk2_queue->size)
+    {
+        enqueue(event_queue, arrival_time, time_disk2_free, id, 2);
+        enqueue(disk2_queue, arrival_time, time_disk2_free, id, 2);
+    }
+    else
+    {
+        enqueue(event_queue, arrival_time, time_disk1_free, id, 1);
+        enqueue(disk1_queue, arrival_time, time_disk1_free, id, 1);
+    }
+}
+
+int disk_handler(Node **job)
+{
+    int finished_time;
+    if ((*job)->server == 1)
+    {
+        int id = dequeue(disk1_queue);
+        finished_time = process_disk1(job);
+        time_disk1_free = finished_time;
+
+        int response_time = time_disk1_free - ((*job)->arrival_time);
+        int process_time = time_disk1_free - ((*job)->pop_time);
+        log_statistics((*job)->id, (*job)->server, (*job)->arrival_time, (*job)->pop_time, time_disk1_free,
+                       response_time, process_time, subSize(disk1_queue, time_disk1_free));
+
+        if (disk1_queue->head)
+        {
+            disk1_queue->head->pop_time = time_disk1_free;
+        }
+
+        enqueue(event_queue, finished_time, time_cpu_free, id, 0);
+        enqueue(cpu_queue, finished_time, time_cpu_free, id, 0);
+    }
+    else
+    {
+        int id = dequeue(disk2_queue);
+        finished_time = process_disk2(job);
+        time_disk2_free = finished_time;
+        
+        if (disk2_queue->head)
+        {
+            disk2_queue->head->pop_time = time_disk2_free;
+        }
+
+        int response_time = time_disk2_free - ((*job)->arrival_time);
+        int process_time = time_disk2_free - ((*job)->pop_time);
+        log_statistics((*job)->id, (*job)->server, (*job)->arrival_time, (*job)->pop_time, time_disk2_free,
+                       response_time, process_time, subSize(disk2_queue, time_disk2_free));
+
+        enqueue(event_queue, finished_time, time_cpu_free, id, 0);
+        enqueue(cpu_queue, finished_time, time_cpu_free, id, 0);
+    }
+    return finished_time;
+}
+
+int process_disk1(Node **job)
+{
+    int current_time = (*job)->pop_time;
+    if (current_time < time_disk1_free)
+    {
+        current_time = time_disk1_free;
+        (*job)->pop_time = current_time;
+    }
+    int time_handling = (rand() % (DISK1_MAX - DISK1_MIN)) + DISK1_MIN + 1;
+    return current_time + time_handling;
+}
+
+int process_disk2(Node **job)
+{
+    int current_time = (*job)->pop_time;
+    if (current_time < time_disk2_free)
+    {
+        current_time = time_disk2_free;
+        (*job)->pop_time = current_time;
+    }
+    int time_handling = (rand() % (DISK2_MAX - DISK2_MIN)) + DISK2_MIN + 1;
+    return current_time + time_handling;
+}
+
+/*
+Read, write and calculate statistics function
+*/
+
+void read_config()
+{
+    FILE *f;
+    f = fopen("/Users/hoangho/Documents/document/CS/CIS3207/labs/lab_1/config.txt", "r");
+
+    //read file into array
+
+    if (f == NULL)
+    {
+        printf("Error Reading File\n");
+        exit(0);
+    }
+    fscanf(f, "%d\n", &SEED);
+    fscanf(f, "%d\n", &INIT_TIME);
+    fscanf(f, "%d\n", &FIN_TIME);
+    fscanf(f, "%d\n", &ARRIVE_MIN);
+    fscanf(f, "%d\n", &ARRIVE_MAX);
+    fscanf(f, "%f\n", &QUIT_PROB);
+    fscanf(f, "%d\n", &CPU_MIN);
+    fscanf(f, "%d\n", &CPU_MAX);
+    fscanf(f, "%d\n", &DISK1_MIN);
+    fscanf(f, "%d\n", &DISK1_MAX);
+    fscanf(f, "%d\n", &DISK2_MIN);
+    fscanf(f, "%d\n", &DISK2_MAX);
+    fclose(f);
+}
---
+++ lab_1/statistics.c
@@ -0,0 +1,233 @@
+/*
+author: Hoang Ho
+Statistics File
+*/
+
+#include "statistics.h"
+
+void start_log()
+{
+    FILE *f;
+    f = fopen("/Users/hoangho/Documents/document/CS/CIS3207/labs/lab_1/log_statistics.txt", "w+");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    fprintf(f, "%-15s %-15s %-15s %-15s %-15s %-15s %-15s %-15s\n",
+            "Job_ID", "Server", "Arrival", "Pop_time", "Finished", "Response_Time",
+            "Process_Time", "Len_Queue");
+    fclose(f);
+}
+
+void log_statistics(int id, int server, int arrival_time, int pop_time, int finished_time,
+                    int response_time, int process_time, int queue_len)
+{
+    FILE *f;
+    f = fopen("/Users/hoangho/Documents/document/CS/CIS3207/labs/lab_1/log_statistics.txt", "a+");
+
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    fprintf(f, "%-15d %-15d %-15d %-15d %-15d %-15d %-15d %-15d\n",
+            id, server, arrival_time, pop_time, finished_time, response_time,
+            process_time, queue_len);
+    fclose(f);
+}
+
+void calculate_statistics(int INIT_TIME, int FIN_TIME)
+{
+    FILE *f;
+    f = fopen("/Users/hoangho/Documents/document/CS/CIS3207/labs/lab_1/log_statistics.txt", "a+");
+
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(f, 0, SEEK_SET);
+    fscanf(f, "%*s %*s %*s %*s %*s %*s %*s %*s");
+
+    int server;
+    int jobs_cpu;
+    int jobs_disk1;
+    int jobs_disk2;
+    int finished;
+
+    int max_cpu_queue = 0;
+    int wcpu_queue = 0;
+    int stIntCPU = 0;
+    int temp_len_cpu = 0;
+    int prev_len_cpu = 0;
+    int cpu_utilization = 0;
+    int cpu_response = 0;
+    int max_cpu_response = 0;
+    int cpu_holder1 = 0;
+    int cpu_holder2 = 0;
+
+    int max_disk1_queue = 0;
+    int wdisk1_queue = 0;
+    int stIntDisk1 = 0;
+    int temp_len_disk1 = 0;
+    int prev_len_disk1 = 0;
+    int disk1_utilization = 0;
+    int disk1_response = 0;
+    int max_disk1_response = 0;
+    int disk1_holder1 = 0;
+    int disk1_holder2 = 0;
+
+    int max_disk2_queue = 0;
+    int wdisk2_queue = 0;
+    int stIntDisk2 = 0;
+    int temp_len_disk2 = 0;
+    int prev_len_disk2 = 0;
+    int disk2_utilization = 0;
+    int disk2_response = 0;
+    int max_disk2_response = 0;
+    int disk2_holder1 = 0;
+    int disk2_holder2 = 0;
+
+    while (fscanf(f, "%*d %d %*d %*d", &server) == 1)
+    {
+        if (server == 0)
+        {
+            fscanf(f, "%d %d %d %d", &finished, &cpu_holder1, &cpu_holder2, &temp_len_cpu);
+
+            if (finished > FIN_TIME)
+            {
+                cpu_holder1 -= (finished - FIN_TIME);
+                cpu_holder2 -= (finished - FIN_TIME);
+                finished -= (finished - FIN_TIME);
+            }
+
+            jobs_cpu += 1;
+            cpu_response += cpu_holder1;
+            cpu_utilization += cpu_holder2;
+            if (cpu_holder1 > max_cpu_response)
+            {
+                max_cpu_response = cpu_holder1;
+            }
+            if (prev_len_cpu != temp_len_cpu)
+            {
+                if (stIntCPU == 0)
+                {
+                    stIntCPU = finished;
+                }
+                else
+                {
+                    wcpu_queue += (finished - stIntCPU) * prev_len_cpu;
+                    stIntCPU = finished;
+                }
+            }
+            prev_len_cpu = temp_len_cpu;
+            if (temp_len_cpu > max_cpu_queue)
+            {
+                max_cpu_queue = temp_len_cpu;
+            }
+        }
+        else if (server == 1)
+        {
+            fscanf(f, "%d %d %d %d", &finished, &disk1_holder1, &disk1_holder2, &temp_len_disk1);
+
+            if (finished > FIN_TIME)
+            {
+                disk1_holder1 -= (finished - FIN_TIME);
+                disk2_holder2 -= (finished - FIN_TIME);
+            }
+
+            jobs_disk1 += 1;
+            disk1_response += disk1_holder1;
+            disk1_utilization += disk1_holder2;
+            if (disk1_holder1 > max_disk1_response)
+            {
+                max_disk1_response = disk1_holder1;
+            }
+            if (prev_len_disk1 != temp_len_disk1)
+            {
+                if (stIntDisk1 == 0)
+                {
+                    stIntDisk1 = finished;
+                }
+                else
+                {
+                    wdisk1_queue += (finished - stIntDisk1) * prev_len_cpu;
+                    stIntDisk1 = finished;
+                }
+            }
+            prev_len_disk1 = temp_len_disk1;
+            if (temp_len_disk1 > max_disk1_queue)
+            {
+                max_disk1_queue = temp_len_disk1;
+            }
+        }
+        else
+        {
+            fscanf(f, "%d %d %d %d", &finished, &disk2_holder1, &disk2_holder2, &temp_len_disk2);
+
+            if (finished > FIN_TIME)
+            {
+                disk2_holder1 -= (finished - FIN_TIME);
+                disk2_holder2 -= (finished - FIN_TIME);
+            }
+
+            jobs_disk2 += 1;
+            disk2_response += disk2_holder1;
+            disk2_utilization += disk2_holder2;
+            if (disk2_holder1 > max_disk2_response)
+            {
+                max_disk2_response = disk2_holder1;
+            }
+            if (prev_len_disk2 != temp_len_disk2)
+            {
+                if (stIntDisk2 == 0)
+                {
+                    stIntDisk2 = finished;
+                }
+                else
+                {
+                    wdisk2_queue += (finished - stIntDisk2) * prev_len_cpu;
+                    stIntDisk2 = finished;
+                }
+            }
+            prev_len_disk2 = temp_len_disk2;
+            if (temp_len_disk2 > max_disk2_queue)
+            {
+                max_disk2_queue = temp_len_disk2;
+            }
+        }
+    }
+    wcpu_queue += (FIN_TIME - stIntCPU) * prev_len_cpu;
+    wdisk1_queue +=  (FIN_TIME - stIntDisk1) * prev_len_disk1;
+    wdisk2_queue += (FIN_TIME - stIntDisk2) * prev_len_disk2;
+
+    fprintf(f, "\n");
+    fprintf(f, "CPU utilization: %.2f\n", (1.00 * cpu_utilization) / (FIN_TIME - INIT_TIME));
+    fprintf(f, "Average CPU response: %2.f\n", ((1.00 * cpu_response) / jobs_cpu));
+    fprintf(f, "Max CPU response: %d\n", max_cpu_response);
+    fprintf(f, "Max CPU queue length: %d\n", max_cpu_queue);
+    fprintf(f, "Weighted Average CPU queue length: %.2f\n", (1.00 * wcpu_queue) / (FIN_TIME - INIT_TIME));
+    fprintf(f, "Jobs finished in CPU per one unit of time: %.2f\n", (1.00 * jobs_cpu) / (FIN_TIME - INIT_TIME));
+
+    fprintf(f, "\n");
+    fprintf(f, "Disk 1 utilization: %.2f\n", (1.00 * disk1_utilization) / (FIN_TIME - INIT_TIME));
+    fprintf(f, "Average Disk 1 response: %.2f\n", ((1.00 * disk1_response) / jobs_disk1));
+    fprintf(f, "Max Disk 1 response: %d\n", max_disk1_response);
+    fprintf(f, "Max Disk 1 queue: %d\n", max_disk1_queue);
+    fprintf(f, "Weighted Average Disk 1 queue length: %.2f\n", (1.00 * wdisk1_queue) / (FIN_TIME - INIT_TIME));
+    fprintf(f, "Jobs finished in disk 1 per one unit of time: %.2f\n", (1.00 * jobs_disk1) / (FIN_TIME - INIT_TIME));
+
+    fprintf(f, "\n");
+    fprintf(f, "Disk 2 utilization: %.2f\n", (1.00 * disk2_utilization) / (FIN_TIME - INIT_TIME));
+    fprintf(f, "Average Disk 2 response: %.2f\n", ((1.00 * disk2_response) / jobs_disk2));
+    fprintf(f, "Max Disk 2 response: %d\n", max_disk2_response);
+    fprintf(f, "Max Disk 2 queue: %d\n", max_disk2_queue);
+    fprintf(f, "Weighted Average Disk 2 queue length: %.2f\n", (1.00 * wdisk2_queue) / (FIN_TIME - INIT_TIME));
+    fprintf(f, "Jobs finished in disk 2 per one unit of time: %.2f\n", (1.00 * jobs_disk2) / (FIN_TIME - INIT_TIME));
+
+    fclose(f);
+    exit(EXIT_SUCCESS);
+}
